82da3e5af542c48aab053d6eda89f4091adc8db6
Rasheed <rasheedmohammed2006@gmail.com>
2025-04-04 11:58:27 +1100

    Intial Commit. Import skelton code into project.

---
+++ .idea/DonkeyKong_Skeleton/src/ShadowDonkeyKong.java
@@ -0,0 +1,56 @@
+import bagel.*;
+import java.util.Properties;
+
+/**
+ * The main class for the Shadow Donkey Kong game.
+ * This class extends {@code AbstractGame} and is responsible for managing game initialization,
+ * updates, rendering, and handling user input.
+ *
+ * It sets up the game world, initializes characters, platforms, ladders, and other game objects,
+ * and runs the game loop to ensure smooth gameplay.
+ */
+public class ShadowDonkeyKong extends AbstractGame {
+
+    private final Properties GAME_PROPS;
+    private final Properties MESSAGE_PROPS;
+
+
+    public ShadowDonkeyKong(Properties gameProps, Properties messageProps) {
+        super(Integer.parseInt(gameProps.getProperty("window.width")),
+                Integer.parseInt(gameProps.getProperty("window.height")),
+                messageProps.getProperty("home.title"));
+
+        this.GAME_PROPS = gameProps;
+        this.MESSAGE_PROPS = messageProps;
+    }
+
+
+    /**
+     * Render the relevant screen based on the keyboard input given by the user and the status of the gameplay.
+     * @param input The current mouse/keyboard input.
+     */
+    @Override
+    protected void update(Input input) {
+        if (input.wasPressed(Keys.ESCAPE)) {
+            Window.close();
+        }
+    }
+
+
+    /**
+     * The main entry point of the Shadow Donkey Kong game.
+     *
+     * This method loads the game properties and message files, initializes the game,
+     * and starts the game loop.
+     *
+     * @param args Command-line arguments (not used in this game).
+     */
+    public static void main(String[] args) {
+        Properties gameProps = IOUtils.readPropertiesFile("res/app.properties");
+        Properties messageProps = IOUtils.readPropertiesFile("res/message_en.properties");
+        ShadowDonkeyKong game = new ShadowDonkeyKong(gameProps, messageProps);
+        game.run();
+    }
+
+
+}
