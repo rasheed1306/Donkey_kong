d215c6f706ef6348dbbd02ac9eee62828e743cf9
Rasheed Mohammed <rasheedmohammed2006@gmail.com>
2025-04-15 01:21:28 +1000

    Fix when player climbs ladder

--- DonkeyKong_Skeleton/src/Ladder.java
+++ DonkeyKong_Skeleton/src/Ladder.java
@@ -18,7 +18,7 @@ public class Ladder {
         for (int i = 0; i < ladderCount; i++) {
             String[] parts = gameProps.getProperty(ladders[i]).split(",");
             ladderCoords[i] = new Point(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
-            ladderBounds[i] = new Rectangle(ladderCoords[i].x - ladder.getWidth() / 2, ladderCoords[i].y - ladder.getHeight() / 2 + 30, ladder.getWidth(), ladder.getHeight());
+            ladderBounds[i] = new Rectangle(ladderCoords[i].x, ladderCoords[i].y, ladder.getWidth(), ladder.getHeight() - 30);
         }
     }
 
